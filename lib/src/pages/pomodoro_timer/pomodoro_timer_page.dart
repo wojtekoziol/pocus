896b1d5a93eb6f036e1fecf8c0e378d286859177
Wojtek <wojtek.koziol.pl@gmail.com>
2021-03-22 07:13:19 +0100

    Add pomodoro timer fields representation

--- lib/src/pages/pomodoro_timer/pomodoro_timer_page.dart
+++ lib/src/pages/pomodoro_timer/pomodoro_timer_page.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pocus/providers.dart';
 
 class PomodoroTimerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text('Pomodoro Timer Page'),
+        child: Consumer(
+          builder: (_, watch, __) {
+            final pomodoroTimerState = watch(pomodoroTimerNotifier.state);
+
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('secondsLeft: ${pomodoroTimerState.secondsLeft}'),
+                Text('isRunning: ${pomodoroTimerState.isRunning}'),
+                Text('isBreak: ${pomodoroTimerState.isBreak}'),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
