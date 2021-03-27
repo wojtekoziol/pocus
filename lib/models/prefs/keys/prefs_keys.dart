db63c30eb2ea846db329a0e6a78d3407ff0573ad
Wojtek <wojtek.koziol.pl@gmail.com>
2021-03-27 14:16:40 +0100

    Add prefs keys

---
+++ lib/models/prefs/keys/prefs_keys.dart
@@ -0,0 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'prefs_keys.freezed.dart';
+
+@freezed
+class PrefsKeys with _$PrefsKeys {
+  factory PrefsKeys._() = _PrefsKeys;
+  static String pomodoroTimerState = 'pomodorotimerstate';
+  static String pomodoroDuration = 'pomodoroduration';
+  static String shortBreakDuration = 'shortbreakduration';
+  static String longBreakDuration = 'longbreakduration';
+  static String intervalsNumber = 'intervalsnumber';
+}
