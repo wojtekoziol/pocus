29b706304bb47e317c5cc517c87b1224f708e0d0
Wojtek <wojtek.koziol.pl@gmail.com>
2021-04-01 12:33:51 +0200

    Add restore default settings button

--- lib/src/pages/settings/settings_page.dart
+++ lib/src/pages/settings/settings_page.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:pocus/src/pages/settings/widgets/intervals_number_setting.dart';
-import 'package:pocus/src/pages/settings/widgets/long_break_duration_setting.dart';
-import 'package:pocus/src/pages/settings/widgets/pomodoro_timer_duration_setting.dart';
-import 'package:pocus/src/pages/settings/widgets/short_break_duration_setting.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pocus/providers.dart';
+import 'package:pocus/src/pages/settings/models/radio_slider_option/radio_slider_option.dart';
+import 'package:pocus/src/pages/settings/widgets/default_settings_button.dart';
+import 'package:pocus/src/pages/settings/widgets/radio_slider.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends HookWidget {
+  final _pomodoroTimerOptions = [25, 30, 45];
+  final _shortBreakOptions = [2, 5, 10];
+  final _longBreakOptions = [10, 15, 20];
+  final _intervalsNumberOptions = [2, 4, 6];
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final prefsNotifier = context.read(prefsNotifierProvider);
+    final prefsState = context.read(prefsNotifierProvider.state);
 
     return Scaffold(
       appBar: AppBar(
@@ -27,13 +36,79 @@ class SettingsPage extends StatelessWidget {
         padding: const EdgeInsets.symmetric(vertical: 48, horizontal: 24),
         child: Column(
           children: [
-            PomodoroTimerDurationSetting(),
+            RadioSlider(
+              label: 'Pomodoro timer duration',
+              initialIndex:
+                  _pomodoroTimerOptions.indexOf(prefsState.pomodoroDuration),
+              children: [
+                ..._pomodoroTimerOptions.map((option) {
+                  return RadioSliderOption(
+                    value: '$option min',
+                    onPressed: () {
+                      prefsNotifier.updatePrefs(prefsState.copyWith(
+                        pomodoroDuration: option,
+                      ));
+                    },
+                  );
+                }),
+              ],
+            ),
             SizedBox(height: 50),
-            ShortBreakDurationSetting(),
+            RadioSlider(
+              label: 'Short break duration',
+              initialIndex:
+                  _shortBreakOptions.indexOf(prefsState.shortBreakDuration),
+              children: [
+                ..._shortBreakOptions.map((option) {
+                  return RadioSliderOption(
+                    value: '$option min',
+                    onPressed: () {
+                      prefsNotifier.updatePrefs(prefsState.copyWith(
+                        shortBreakDuration: option,
+                      ));
+                    },
+                  );
+                }),
+              ],
+            ),
             SizedBox(height: 50),
-            LongBreakDurationSetting(),
+            RadioSlider(
+              label: 'Long break duration',
+              initialIndex:
+                  _longBreakOptions.indexOf(prefsState.longBreakDuration),
+              children: [
+                ..._longBreakOptions.map((option) {
+                  return RadioSliderOption(
+                    value: '$option min',
+                    onPressed: () {
+                      prefsNotifier.updatePrefs(prefsState.copyWith(
+                        longBreakDuration: option,
+                      ));
+                    },
+                  );
+                }),
+              ],
+            ),
             SizedBox(height: 50),
-            IntervalsNumberSetting(),
+            RadioSlider(
+              label: 'Pomodoro intervals number',
+              initialIndex:
+                  _intervalsNumberOptions.indexOf(prefsState.intervalsNumber),
+              children: [
+                ..._intervalsNumberOptions.map((option) {
+                  return RadioSliderOption(
+                    value: '$option',
+                    onPressed: () {
+                      prefsNotifier.updatePrefs(prefsState.copyWith(
+                        intervalsNumber: option,
+                      ));
+                    },
+                  );
+                }),
+              ],
+            ),
+            SizedBox(height: 50),
+            DefaultSettingsButton(),
           ],
         ),
       ),
---
+++ lib/src/pages/settings/widgets/default_settings_button.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pocus/models/prefs/state/prefs_state.dart';
+import 'package:pocus/providers.dart';
+
+class DefaultSettingsButton extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final scaleAnimationController =
+        useAnimationController(duration: Duration(milliseconds: 50));
+    final prefsNotifier = context.read(prefsNotifierProvider);
+
+    return ScaleTransition(
+      scale: Tween<double>(
+        begin: 1,
+        end: 0.9,
+      ).animate(scaleAnimationController),
+      child: GestureDetector(
+        onTapDown: (details) {
+          scaleAnimationController.forward();
+        },
+        onTapUp: (details) {
+          prefsNotifier.updatePrefs(PrefsState(
+            pomodoroDuration: 25,
+            shortBreakDuration: 5,
+            longBreakDuration: 15,
+            intervalsNumber: 4,
+          ));
+          scaleAnimationController.reverse();
+          Navigator.of(context).pop();
+        },
+        onTapCancel: () {
+          scaleAnimationController.reverse();
+        },
+        child: FractionallySizedBox(
+          widthFactor: 0.6,
+          child: Container(
+            height: 40,
+            decoration: BoxDecoration(
+              color: theme.primaryColor,
+              borderRadius: BorderRadius.circular(32),
+            ),
+            child: Center(
+              child: Text(
+                'Restore to default',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
