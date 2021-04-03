dec71f362b57e635cddf273fddf639963bec0521
Wojtek <wojtek.koziol.pl@gmail.com>
2021-04-03 14:56:05 +0200

    Add animations to stats chart

--- lib/src/pages/stats/widgets/stats_bar_chart.dart
+++ lib/src/pages/stats/widgets/stats_bar_chart.dart
@@ -2,8 +2,9 @@ import 'dart:math';
 
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class StatsBarChart extends StatelessWidget {
+class StatsBarChart extends HookWidget {
   StatsBarChart({
     required this.stats,
     required this.backgroundColor,
@@ -25,6 +26,7 @@ class StatsBarChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final touchedIndexNotifier = useValueNotifier(-1);
 
     return Container(
       decoration: BoxDecoration(
@@ -44,44 +46,68 @@ class StatsBarChart extends StatelessWidget {
           ),
           SizedBox(height: 16),
           Expanded(
-            child: BarChart(
-              BarChartData(
-                barTouchData: BarTouchData(
-                    touchTooltipData: BarTouchTooltipData(
-                  tooltipRoundedRadius: 16,
-                  tooltipBgColor: tooltipColor,
-                  getTooltipItem: (group, groupIndex, rod, rodIndex) {
-                    return BarTooltipItem(
-                      '${stats[groupIndex]}',
-                      theme.textTheme.subtitle2!.copyWith(
-                        color: backgroundColor,
-                        fontWeight: FontWeight.bold,
+            child: ValueListenableBuilder(
+              valueListenable: touchedIndexNotifier,
+              builder: (context, touchedIndex, child) {
+                return BarChart(
+                  BarChartData(
+                    barTouchData: BarTouchData(
+                      touchTooltipData: BarTouchTooltipData(
+                        tooltipRoundedRadius: 12,
+                        tooltipBgColor: tooltipColor.withOpacity(0.9),
+                        getTooltipItem: (group, groupIndex, rod, rodIndex) {
+                          return BarTooltipItem(
+                            '${stats[groupIndex]}',
+                            theme.textTheme.subtitle2!.copyWith(
+                              color: backgroundColor,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          );
+                        },
                       ),
-                    );
-                  },
-                )),
-                barGroups: [
-                  for (int i = 0; i < stats.length; i++)
-                    BarChartGroupData(
-                      x: i,
-                      barRods: [
-                        BarChartRodData(
-                          y: stats[i].toDouble(),
-                          width: 20,
-                          colors: [lineColor],
-                          backDrawRodData: BackgroundBarChartRodData(
-                            colors: [backgroundLineColor],
-                            show: true,
-                            y: max(stats.reduce(max).toDouble(), 1),
-                          ),
-                        ),
-                      ],
-                    )
-                ],
-                alignment: BarChartAlignment.center,
-                borderData: FlBorderData(show: false),
-                titlesData: FlTitlesData(show: false),
-              ),
+                      touchCallback: (barTouchResponse) {
+                        if (barTouchResponse.spot == null ||
+                            barTouchResponse.touchInput is PointerUpEvent) {
+                          touchedIndexNotifier.value = -1;
+                          return;
+                        }
+                        touchedIndexNotifier.value =
+                            barTouchResponse.spot!.touchedBarGroupIndex;
+                      },
+                    ),
+                    barGroups: [
+                      for (int i = 0; i < stats.length; i++)
+                        BarChartGroupData(
+                          x: i,
+                          barRods: [
+                            BarChartRodData(
+                              y: touchedIndex == i
+                                  ? max(
+                                      stats.reduce(max) * 0.75, stats[i] * 1.2)
+                                  : stats[i].toDouble(),
+                              width: 20,
+                              colors: [
+                                touchedIndex == -1
+                                    ? lineColor
+                                    : touchedIndex == i
+                                        ? lineColor
+                                        : Colors.white,
+                              ],
+                              backDrawRodData: BackgroundBarChartRodData(
+                                colors: [backgroundLineColor],
+                                show: true,
+                                y: stats.reduce(max) * 1.2,
+                              ),
+                            ),
+                          ],
+                        )
+                    ],
+                    alignment: BarChartAlignment.center,
+                    borderData: FlBorderData(show: false),
+                    titlesData: FlTitlesData(show: false),
+                  ),
+                );
+              },
             ),
           ),
         ],
